d122864252b662db611a813301df5b52045be86e
Jesse Nickles <jesse@littlebizzy.com>
2018-08-25 01:58:10 -0500

    Update delete-expired-transients.php

--- delete-expired-transients.php
+++ delete-expired-transients.php
@@ -3,7 +3,7 @@
 Plugin Name: Delete Expired Transients
 Plugin URI: https://www.littlebizzy.com/plugins/delete-expired-transients
 Description: Deletes all expired transients upon activation and on a daily basis thereafter via WP-Cron to maintain a cleaner database and improve performance.
-Version: 1.0.2
+Version: 1.0.3
 Author: LittleBizzy
 Author URI: https://www.littlebizzy.com
 License: GPLv3
@@ -17,7 +17,7 @@ namespace LittleBizzy\DeleteExpiredTransients;
 // Plugin constants
 const FILE = __FILE__;
 const PREFIX = 'dlextr';
-const VERSION = '1.0.2';
+const VERSION = '1.0.3';
 
 // Block direct calls
 if (!function_exists('add_action'))
